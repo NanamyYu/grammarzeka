0055c817519e1d363f5467d6e539e00e9bcd7984
NanamyYu <lizashatskaja@mail.ru>
2022-07-12 18:11:51 +0300

    Update buttons

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет! Меня зовут Граммазека, я бот, который проверяет знания русского языка с помощью викторин. Чтобы узнать, что я могу, набери ```/help```.')
+    bot.send_message(message.chat.id, 'Привет\! Меня зовут Граммазека, я бот, который проверяет знания русского языка с помощью викторин\. Чтобы узнать, что я могу, набери `/help`\.', parse_mode='MarkdownV2')
 
 @bot.message_handler(commands=['help'])
 def help(message):
@@ -17,15 +17,34 @@ def help(message):
 
 @bot.message_handler(commands=['quiz'])
 def quiz(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    answer1=types.KeyboardButton("Ответ 1")
-    answer2=types.KeyboardButton("Ответ 2")
-    answer3=types.KeyboardButton("Ответ 3")
-    answer4=types.KeyboardButton("Ответ 4")
-    markup.add(answer1)
-    markup.add(answer2)
-    markup.add(answer3)
-    markup.add(answer4)
-    bot.send_message(message.chat.id, 'Вопрос', reply_markup=markup)
+    start_quiz=types.InlineKeyboardMarkup()
+    start_button=types.InlineKeyboardButton(text="Старт", callback_data="skip")
+    start_quiz.add(start_button)
+    bot.send_message(chat_id=message.chat.id, text="Начнем викторину! Я буду присылать предложения с пропущенным словом, а Вам надо будет выбрать, какой вариант пропущенного слова верен.", reply_markup=start_quiz)
+
+
+def question(call):
+    markup=types.InlineKeyboardMarkup()
+    answer1=types.InlineKeyboardButton(text="Неправильный ответ", callback_data="False")
+    answer2=types.InlineKeyboardButton(text="Правильный ответ", callback_data="True")
+    answer3=types.InlineKeyboardButton(text="Неправильный ответ", callback_data="False")
+    answer4=types.InlineKeyboardButton(text="Неправильный ответ", callback_data="False")
+    skip_button=types.InlineKeyboardButton(text="Пропустить", callback_data="skip")
+    markup.add(answer1, answer2, answer3, answer4, skip_button)
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Вопрос", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        if call.data == "True" or call.data == "False":
+            next_q=types.InlineKeyboardMarkup()
+            next_button=types.InlineKeyboardButton(text="Следующий вопрос", callback_data="skip")
+            next_q.add(next_button)
+            if call.data == "True":
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Молодец.", reply_markup=next_q)
+            else:
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Дурак, что ли? Написано же, неправильный ответ. Зачем сюда нажимать вообще?", reply_markup=next_q)
+        elif call.data == "skip":
+            question(call)
 
 bot.infinity_polling()

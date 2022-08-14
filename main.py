14b3cda475ebbd2aaf931d04f1e01981194212fe
NanamyYu <lizashatskaja@mail.ru>
2022-08-14 20:51:16 +0300

    Connect with sentences

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ import random
 import json
 import os
 import logging
+import jsonlines
+import pandas
 
 token = os.getenv("GRAMMARZEKA_TOKEN")
 bot = telebot.TeleBot(token)
-user_id = [int(os.getenv("user_id"))]
 
 # Base logger
 logger = logging.getLogger('grammarzeka')
 logger.setLevel(logging.INFO)
 logging.basicConfig(filename = "mylog.log", format = "%(asctime)s\t%(levelname)s\t%(funcName)s\t%(lineno)d\t%(message)s")
 
-logger.info("Bot started")
 
-questions = ["В новогоднюю ночь будет много ____.", "____ нужно делать обследование всего организма.", "На избирательном участке нам выдали специальные ____, которые нужно было заполнить и опустить в тумбу."]
-answers = [["Феерверков", "Фейерверков", "Феирверков", "Фейирверков"], ["Переодически", "Переодичиски", "Периодически", "Периодичиски"], ["Бюлетени", "Бюллитени", "Бюллетени", "Бюлитени"]]
-answersmask = [1, 2, 2]
+# Загрузка сообщений
+with jsonlines.open('labs/jsonl/alisa_selezneva.jsonl') as f:
+    sentences = pandas.DataFrame(f)
+
+# questions = ["В новогоднюю ночь будет много ____.", "____ нужно делать обследование всего организма.", "На избирательном участке нам выдали специальные ____, которые нужно было заполнить и опустить в тумбу."]
+# answers = [["Феерверков", "Фейерверков", "Феирверков", "Фейирверков"], ["Переодически", "Переодичиски", "Периодически", "Периодичиски"], ["Бюлетени", "Бюллитени", "Бюллетени", "Бюлитени"]]
+# answersmask = [1, 2, 2]
+
+
+# Default buttons
+end_button=types.InlineKeyboardButton(text="Закончить", callback_data="end")
+
 
 with open("message_templates.json", "r") as f:
     temp_text = json.load(f)
@@ -41,55 +50,52 @@ def quiz(message):
 
 
 def question(call, exc=None):
-    numquest = [0, 1, 2]
-    if exc != None:
-        numquest.remove(exc)
-    num = random.choice(numquest)
+    # numquest = [0, 1, 2]
+    # if exc != None:
+    #     numquest.remove(exc)
+    num = random.choice(range(15223))
     markup=types.InlineKeyboardMarkup()
-    for i in range(4):
-        if i == answersmask[num]:
-            markup.add(types.InlineKeyboardButton(text=answers[num][i], callback_data="T"+str(num)))
-        else:
-            markup.add(types.InlineKeyboardButton(text=answers[num][i], callback_data="F"+str(num)))
+    numword = random.choice(range(len(sentences['complex_words'][num])))
+    markup.add(types.InlineKeyboardButton(text=sentences['complex_words'][num][numword]['word'], callback_data="T"))
+    for i in range(len(sentences['complex_words'][num][numword]['distortions'])):
+        markup.add(types.InlineKeyboardButton(text=sentences['complex_words'][num][numword]['distortions'][i], callback_data="F"))
     skip_button=types.InlineKeyboardButton(text="Пропустить", callback_data="skip"+str(num))
-    end_button=types.InlineKeyboardButton(text="Закончить", callback_data="end")
     easy_button=types.InlineKeyboardButton(text="Слишком простой вопрос", callback_data="easy"+str(num))
     markup.add(skip_button, easy_button, end_button)
-    bot.send_message(chat_id=call.message.chat.id, text=questions[num], reply_markup=markup)
+    bot.send_message(chat_id=call.message.chat.id, text=sentences['sentence'][num].replace(sentences['complex_words'][num][numword]['word'],  "\_\_\_\_"), reply_markup=markup, parse_mode='Markdown')
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.message:
         if call.data[0] == "T" or call.data[0] == "F":
-            num = int(call.data[1])
+            # num = int(call.data[1])
             next_q=types.InlineKeyboardMarkup()
-            next_button=types.InlineKeyboardButton(text="Следующий вопрос", callback_data="skip"+str(num))
-            end_button=types.InlineKeyboardButton(text="Закончить", callback_data="end")
+            next_button=types.InlineKeyboardButton(text="Следующий вопрос", callback_data="skip")
             next_q.add(next_button, end_button)
-            right_answer = str(answers[num][answersmask[num]])
             if call.data[0] == "T":
-                logger.info("Passed question" + str(num + 1))
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="*Правильно!* \n\nПолностью предложение звучит так: \n\n_" + str(questions[num]).replace("____", right_answer) + "_\n\nПродолжим?", reply_markup=next_q, parse_mode='Markdown')
+                get_message_for_logfile("Question passed")
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="*Правильно!*\n\nПродолжим?", reply_markup=next_q, parse_mode='Markdown')
             else:
-                logger.info("Failed question" + str(num + 1))
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="*Неверно.* \n\nПравильный ответ: *" + right_answer + "*, а полностью предложение звучит так: \n\n_" + str(questions[num]).replace("____", right_answer) + "_\n\nПродолжим?", reply_markup=next_q, parse_mode='Markdown')
+                get_message_for_logfile("Question failed")
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="*Неверно.*\n\nПродолжим?", reply_markup=next_q, parse_mode='Markdown')
         elif call.data[:4] == "skip":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text, parse_mode='Markdown')
             if len(call.data) > 4:
                 question(call, int(call.data[4]))
             else:
                 question(call)
         elif call.data == "end":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text, parse_mode='Markdown')
             bot.send_message(chat_id=call.message.chat.id, text=temp_text["quiz_end"])
         elif call.data[:4] == "easy":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text, parse_mode='Markdown')
             num = int(call.data[4])
-            logger.info("Easy question\t" + str(num + 1))
+            get_message_for_logfile("Easy question", num + 1)
             question(call, num)
-
-bot.infinity_polling()
-
 
 def get_message_for_logfile(message, user_id=None, num=None):
     logger.info(message + "\t" + str(num))
+
+
+bot.infinity_polling()
+get_message_for_logfile("Bot started")
